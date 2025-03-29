af5f1fd73ff0fa12502a809735d409cd9b31349b
hyewon <hw2938@gmail.com>
2025-03-30 02:22:54 +0900

    마이페이지 라우터 적용 및 마이페이지 컴포넌트 경로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; // Link import 추가
 import Mypage from "./Components/Mypage/Mypage"; // Mypage 컴포넌트 불러오기
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Mypage />
-    </div>
+    <BrowserRouter>
+      <nav>
+        {/* 메인 페이지와 마이페이지로 이동할 수 있는 링크 
+            임시라 메인 병합시 삭제될 예정            */}
+        <Link to="/">메인 페이지</Link>
+        <Link to="/mypage">마이페이지</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>메인 페이지</h1>} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
